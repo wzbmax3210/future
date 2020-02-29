d077394b0c94d6315f089fe78fb3f17a35d73680
max <1127979416@qq.com>
2020-02-29 10:34:35 +0800

    add self jquery api code

---
+++ javascript/jquery-self/main.js
@@ -0,0 +1,5 @@
+jquery('.test1').find('.children').addClass('fucker').end().addClass('damn')
+
+jquery('.test1').find('.children').parent().print().addClass('oh')
+
+jquery('.test1').children().print()
